64cb5818344ef23809fce810072310198f0d7362
daichao <380967521@qq.com>
2024-10-24 13:11:59 +0800

    1.0.7

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# dcLib_baseLibrary
+# dcLib_baseLibrary 
+# 本地编译生成.aar： ./gradlew install 
+# 本地编译生成.aar： ./gradlew build 
+# 生成的.aar 文件通常会在 library/build/outputs/aar/ 目录下。
 # 本地编译：./gradlew publishToMavenLocal
 # 引用的项目的根build.gradle 配置 maven { url 'https://jitpack.io' }
--- dcLib_baseLibrary/src/main/java/com/dclib/dclib/baselibrary/util/ContactPersonUtil.java
+++ dcLib_baseLibrary/src/main/java/com/dclib/dclib/baselibrary/util/ContactPersonUtil.java
@@ -40,38 +40,38 @@ public class ContactPersonUtil {
         if (cursor != null) {
             while (cursor.moveToNext()) {
 
-                // 联系人ID
-                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-
-                // 获得联系人姓名
-                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-
-                // 获得联系人手机号码
-                Cursor phoneCursor = resolver.query(
-                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "="
-                                + id, null, null);
-
-                // 取得电话号码(可能存在多个号码) 因为同一个名字下，用户可能存有一个以上的号，遍历。
-                String phoneNumber = "";
-                if (phoneCursor != null) {
-                    while (phoneCursor.moveToNext()) {
-                        int phoneFieldColumnIndex = phoneCursor
-                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-                        phoneNumber = phoneCursor.getString(phoneFieldColumnIndex);
-                        // 有的手机号中间会带有空格
-                        phoneNumber = phoneNumber.replace(" ", "");
-                    }
-                    phoneCursor.close();
-                }
-
-                ContactPersonVo contactPersonVO = new ContactPersonVo();
-                contactPersonVO.setName(name);
-//                contactPersonVO.setFirstLetter(Pinyin4jUtil.getFirstSpell(name));
-//                contactPersonVO.setPinYin(Pinyin4jUtil.getPingYin(name));
-                contactPersonVO.setPhone(phoneNumber);
-
-                list.add(contactPersonVO);
+//                // 联系人ID
+//                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+//
+//                // 获得联系人姓名
+//                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+//
+//                // 获得联系人手机号码
+//                Cursor phoneCursor = resolver.query(
+//                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+//                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "="
+//                                + id, null, null);
+//
+//                // 取得电话号码(可能存在多个号码) 因为同一个名字下，用户可能存有一个以上的号，遍历。
+//                String phoneNumber = "";
+//                if (phoneCursor != null) {
+//                    while (phoneCursor.moveToNext()) {
+//                        int phoneFieldColumnIndex = phoneCursor
+//                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+//                        phoneNumber = phoneCursor.getString(phoneFieldColumnIndex);
+//                        // 有的手机号中间会带有空格
+//                        phoneNumber = phoneNumber.replace(" ", "");
+//                    }
+//                    phoneCursor.close();
+//                }
+//
+//                ContactPersonVo contactPersonVO = new ContactPersonVo();
+//                contactPersonVO.setName(name);
+////                contactPersonVO.setFirstLetter(Pinyin4jUtil.getFirstSpell(name));
+////                contactPersonVO.setPinYin(Pinyin4jUtil.getPingYin(name));
+//                contactPersonVO.setPhone(phoneNumber);
+//
+//                list.add(contactPersonVO);
             }
 
             cursor.close();
